556822c2a94b0e9eec58dd2d213d4961020707d3
Jason Singh <Jason.Singh@baruchmail.cuny.edu>
2018-11-08 20:54:57 -0500

    fixed function

--- pokemon.js
+++ pokemon.js
@@ -16,15 +16,35 @@ function getCharizard(){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
-  data = JSON.parse(this.responseText)
-  console.log(data.abilities.length);
-     for(prop in data) {
-        let info = new Info(data[prop]['name'],data.stats[5][prop],data.stats[4][prop],data.stats[3][prop],data.abilities.length);
-        Pokemon.push(info);
+    data = JSON.parse(this.responseText)
+    console.log(data);
+     // for(prop in data) {
+      let info = new Info(data['name'],data.stats[5].base_stat,data.stats[4].base_stat,data.stats[3].base_stat,data.abilities);
+      Pokemon.push(info);
     }
+  }
 
-   }
-  };
+   // }
+
+
 xhttp.open('GET','http://fizal.me/pokeapi/api/v2/name/charizard.json', true);
 xhttp.send()
 }
+
+
+// function getDitto() {
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function () {
+//   if (this.readyState == 4 && this.status == 200) {
+//   data = JSON.parse(this.responseText)
+//   console.log(data.stats[4].base_stat);
+//   for(prop in data) {
+//     let info = new Info(data.name[prop],data.stats[5].base_stat[prop],data.stats[4].base_stat[prop],data.stats[3].base_stat[prop],data.abilities[prop]);
+//     Pokemon.push(info);
+//   }
+// }
+// };
+//
+// xhttp.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto/', true);
+// xhttp.send()
+// }
