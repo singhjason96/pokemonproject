5de50986a2981e7eab2cad3473559d4fadc80613
Jason Singh <Jason.Singh@baruchmail.cuny.edu>
2018-11-09 15:18:01 -0500

    function fixed

--- pokemon.js
+++ pokemon.js
@@ -1,4 +1,4 @@
-const Pokemon = [];
+var Pokemon = [];
 
 let trainer = 'Bason';
 
@@ -12,39 +12,69 @@ class Info{
   }
 }
 
-function getCharizard(){
+
+// var links = ['http://fizal.me/pokeapi/api/v2/name/charizard', 'https://pokeapi.co/api/v2/pokemon/ditto/.'];
+// var i;
+//
+// for(i=0; i < links.length; i++) {
+
+let pokemon = ['charizard', 'ditto', 'eevee'];
+
+
+// for(i=0; i < links.length; i++) {
+function getPokemon(name){
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    data = JSON.parse(this.responseText)
-    console.log(data);
-     // for(prop in data) {
-      let info = new Info(data['name'],data.stats[5].base_stat,data.stats[4].base_stat,data.stats[3].base_stat,data.abilities);
-      Pokemon.push(info);
-    }
-  }
-
-   // }
-
-
-xhttp.open('GET','http://fizal.me/pokeapi/api/v2/name/charizard.json', true);
-xhttp.send()
+   xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+      data = JSON.parse(this.responseText)
+      console.log(data);
+       let info = new Info(data['name'],data.stats[5].base_stat,data.stats[4].base_stat,data.stats[3].base_stat,data.abilities);
+       Pokemon.push(info);
+      }
+  };
+  xhttp.open('GET', 'http://fizal.me/pokeapi/api/v2/name/' + name + '.json');
+  //xhttp.open('GET', `https://pokeapi.co/api/v2/pokemon/${pokemon[i]}/`, true);
+  xhttp.send();
 }
 
+for (let i=0; i < pokemon.length; i++) {
+  getPokemon(pokemon[i]);
+}
 
 // function getDitto() {
-//   var xhttp = new XMLHttpRequest();
+//  var xhttp = new XMLHttpRequest();
 //   xhttp.onreadystatechange = function () {
 //   if (this.readyState == 4 && this.status == 200) {
 //   data = JSON.parse(this.responseText)
-//   console.log(data.stats[4].base_stat);
-//   for(prop in data) {
-//     let info = new Info(data.name[prop],data.stats[5].base_stat[prop],data.stats[4].base_stat[prop],data.stats[3].base_stat[prop],data.abilities[prop]);
-//     Pokemon.push(info);
+//   console.log(data);
+//   let info = new Info(data['name'],data.stats[5].base_stat,data.stats[4].base_stat,data.stats[3].base_stat,data.abilities);
+//   Pokemon.push(info);
 //   }
-// }
+//
 // };
 //
 // xhttp.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto/', true);
 // xhttp.send()
+// }
+//
+// function getEevee() {
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function () {
+//   if (this.readyState == 4 && this.status == 200) {
+//   data = JSON.parse(this.responseText)
+//   console.log(data);
+//   let info = new Info(data['name'],data.stats[5].base_stat,data.stats[4].base_stat,data.stats[3].base_stat,data.abilities);
+//   Pokemon.push(info);
+//   }
+//
+// };
+//
+// xhttp.open('GET', 'https://pokeapi.co/api/v2/pokemon/eevee/', true);
+// xhttp.send()
+// }
+//
+// function bringToScreen() {
+//   let node = ('h3');
+//   node.innerHTML = pokemon['info'];
+//   document.getElementById('pokemon').appendChilde(node);
 // }
